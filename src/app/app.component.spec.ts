519ae2207ca6ae9ba9ea7ec13e1ba61780c688ea
Aldrin Espinosa! <aldrin@eglow.com>
2022-02-07 11:48:29 -0600

    Se cambió el titulo y los test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testing-angular'`, () => {
+  it(`should have as title 'Testing Angular Tutorial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-angular');
+    expect(app.title).toEqual('Testing Angular Tutorial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Testing Angular Tutorial app is running!');
   });
 });
